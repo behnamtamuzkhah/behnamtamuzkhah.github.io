f2e0dd948cca2a5002fba054e757e138532f9fa3
behnamtamuzkhah <behnam.tamuzkhah@gmail.com>
2026-03-22 02:20:18 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,36 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
+const CACHE_NAME = "ticket-app-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json"
+];
+
+// نصب Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener("activate", () => {
-  clients.claim();
+// فعال‌سازی
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// پاسخ‌دهی آفلاین
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
